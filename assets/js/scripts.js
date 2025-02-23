e1ac56a8be1b013955cd0de062976f0ea32cb58b
JunhaumHayden <junhaumhayden@hotmail.com>
2025-02-23 05:31:18 -0300

    adicionado novos arquivos de imagem e estilos, removidos arquivos obsoletos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,25 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
 
-const getCats = async () => {
-	try {
-		const data = await fetch(BASE_URL);
-		const json = await data.json();
-		return json[0].url;
-	} catch (e) {
-		console.log(e.message);
-	}
-};
+        const getCats = async () => {
+            try {
+                const data = await fetch(BASE_URL);
+                const json = await data.json();
+                return json[0].url;
+            } catch (e) {
+                console.log(e.message);
+            }
+        };
 
-const loadImg = async () => {
-	const img = document.getElementsByTagName('img')[0];
-	img.src = await getCats();
-};
+        const loadImg = async () => {
+			const img = document.getElementById('cat');
+			img.classList.add('loading');
+			img.src = await getCats();
+			img.onload = () => img.classList.remove('loading');
+		};
 
-loadImg();
+        loadImg();
 
-const btn = document.getElementById('change-cat');
-btn.addEventListener('click', loadImg);
+        const btn = document.getElementById('change-cat');
+        btn.addEventListener('click', loadImg);
+
+		
